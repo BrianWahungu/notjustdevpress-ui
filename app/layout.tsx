59ebb7b52c7e626e5a8afe25b1af603de7387d41
BrianWahungu <cepteinhawks@gmail.com>
2024-09-05 09:55:27 +0300

    admin dashboard complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/components/providers/ThemeProvider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "NotJustDevPress",
-  description: "Admin Dashboard",
+  title: 'NotJustDevPress',
+  description: 'Admin dashboard',
 };
 
 export default function RootLayout({
@@ -16,10 +17,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Navbar />
-        {children}</body>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem={true}
+          storageKey='dashboard-theme'
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
